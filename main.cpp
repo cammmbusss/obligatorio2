de37b1beb985c0df41f149f7e47e35da91a8c76d
Juan <jprodbor@gmail.com>
2026-06-21 23:47:34 -0300

    subo lo principal del xml

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "Pyramid.h"
 #include "Box.h"
 #include <algorithm>
+#include "tinyxml2.h"
 
 
 
@@ -68,14 +69,32 @@ void saveBMP(const char* filename, int width, int height, Color* data) {
 }
 
 int main() {
-    int width = 800;
-    int height = 600;
+    using namespace tinyxml2;
 
+    // 1. Cargar XML
+    XMLDocument doc;
+    doc.LoadFile("scene.xml");
+    XMLElement* root = doc.FirstChildElement("scene");
 
+    // 2. Resolución
+    XMLElement* resElem = root->FirstChildElement("resolution");
+    int width = resElem->IntAttribute("width");
+    int height = resElem->IntAttribute("height");
     Color* framebuffer = new Color[width * height];
+
     Scene scene;
 
+    // 3. Cámara
+    XMLElement* camElem = root->FirstChildElement("camera")->FirstChildElement("position");
+    float cx = camElem->FloatAttribute("x");
+    float cy = camElem->FloatAttribute("y");
+    float cz = camElem->FloatAttribute("z");
+    Camera camera(Vec3(cx, cy, cz));
 
+
+
+
+    /*
     // Piso
     scene.objects.push_back(new Plane(
         Vec3(0, -2, 0), Vec3(0, 1, 0),   // piso en y = -2
@@ -109,6 +128,7 @@ int main() {
 
     float alturaPata = 1.0f;
 	float alturaMesa = 0.2f;
+
     // Tabla: ancho y plano, apoyado sobre las patas
     // y va de -1.3 a -1.0  (cerca del piso pero elevado)
     scene.objects.push_back(new Box(
@@ -145,7 +165,49 @@ int main() {
         Vec3(1.7f, -2.0f + alturaPata, -8.0f),
         colorMesa
     ));
+    */
+    // 5. Objetos
+    XMLElement* objectsElem = root->FirstChildElement("objects");
 
+    XMLElement* planeElem = objectsElem->FirstChildElement("plane");
+    while (planeElem) {
+        float px = planeElem->FloatAttribute("px");
+        float py = planeElem->FloatAttribute("py");
+        float pz = planeElem->FloatAttribute("pz");
+        float nx = planeElem->FloatAttribute("nx");
+        float ny = planeElem->FloatAttribute("ny");
+        float nz = planeElem->FloatAttribute("nz");
+        float r = planeElem->FloatAttribute("r");
+        float g = planeElem->FloatAttribute("g");
+        float b = planeElem->FloatAttribute("b");
+        scene.objects.push_back(new Plane(Vec3(px, py, pz), Vec3(nx, ny, nz), Color(r, g, b)));
+        planeElem = planeElem->NextSiblingElement("plane");
+    }
+    // Mesa
+    XMLElement* tableElem = objectsElem->FirstChildElement("table");
+    if (tableElem) {
+        float alturaPata = tableElem->FloatAttribute("alturaPata");
+        float alturaMesa = tableElem->FloatAttribute("alturaMesa");
+        float r = tableElem->FloatAttribute("colorR");
+        float g = tableElem->FloatAttribute("colorG");
+        float b = tableElem->FloatAttribute("colorB");
+        Color colorMesa(r, g, b);
+
+        // tablero
+        scene.objects.push_back(new Box(
+            Vec3(-1.8f, -2.0f + alturaPata, -8.5f),
+            Vec3(1.8f, -2.0f + alturaPata + alturaMesa, -4.5f),
+            colorMesa
+        ));
+        // patas (ejemplo, podés agregar todas)
+        scene.objects.push_back(new Box(
+            Vec3(-1.7f, -2.0f, -5.2f),
+            Vec3(-1.4f, -2.0f + alturaPata, -4.9f),
+            colorMesa
+        ));
+    }
+
+/*
     // =====================
     // OBJETOS SOBRE LA MESA
     // =====================
@@ -179,15 +241,69 @@ int main() {
         Color(1.0f, 1.0f, 1.0f),
         0.95f, 0.0f, 1.0f
     ));
+    */
+        XMLElement* sphereElem = objectsElem->FirstChildElement("sphere");
+        while (sphereElem) {
+            float x = sphereElem->FloatAttribute("cx");
+            float y = sphereElem->FloatAttribute("cy");
+            float z = sphereElem->FloatAttribute("cz");
+            float radius = sphereElem->FloatAttribute("radius");
+            float r = sphereElem->FloatAttribute("r");
+            float g = sphereElem->FloatAttribute("g");
+            float b = sphereElem->FloatAttribute("b");
+            float refl = sphereElem->FloatAttribute("reflection");
+            float transp = sphereElem->FloatAttribute("transparency");
+            float refr = sphereElem->FloatAttribute("refraction");
+
+            scene.objects.push_back(new Sphere(Vec3(x, y, z), radius, Color(r, g, b), refl, transp, refr));
+
+            sphereElem = sphereElem->NextSiblingElement("sphere");
+        }
+        XMLElement* pyrElem = objectsElem->FirstChildElement("pyramid");
+        while (pyrElem) {
+            float x = pyrElem->FloatAttribute("cx");
+            float y = pyrElem->FloatAttribute("cy");
+            float z = pyrElem->FloatAttribute("cz");
+            float size = pyrElem->FloatAttribute("size");
+            float r = pyrElem->FloatAttribute("r");
+            float g = pyrElem->FloatAttribute("g");
+            float b = pyrElem->FloatAttribute("b");
+            scene.objects.push_back(new Pyramid(Vec3(x, y, z), size, Color(r, g, b)));
+            pyrElem = pyrElem->NextSiblingElement("pyramid");
+        }
 
     //LUCES
+    // 4. Luces
+    XMLElement* lightsElem = root->FirstChildElement("lights")->FirstChildElement("light");
+    while (lightsElem) {
+        float lx = lightsElem->FloatAttribute("x");
+        float ly = lightsElem->FloatAttribute("y");
+        float lz = lightsElem->FloatAttribute("z");
+        float intensity = lightsElem->FloatAttribute("intensity");
+        scene.lights.push_back(Light(Vec3(lx, ly, lz), intensity));
+        lightsElem = lightsElem->NextSiblingElement("light");
+    }
+    XMLElement* boxElem = objectsElem->FirstChildElement("box");
+    while (boxElem) {
+        float minx = boxElem->FloatAttribute("minx");
+        float miny = boxElem->FloatAttribute("miny");
+        float minz = boxElem->FloatAttribute("minz");
+        float maxx = boxElem->FloatAttribute("maxx");
+        float maxy = boxElem->FloatAttribute("maxy");
+        float maxz = boxElem->FloatAttribute("maxz");
+        float r = boxElem->FloatAttribute("r");
+        float g = boxElem->FloatAttribute("g");
+        float b = boxElem->FloatAttribute("b");
+        scene.objects.push_back(new Box(Vec3(minx, miny, minz), Vec3(maxx, maxy, maxz), Color(r, g, b)));
+        boxElem = boxElem->NextSiblingElement("box");
+    }
 
-    scene.lights.push_back(Light(Vec3(0.0f, 1.0f, -5.8f), 3.5f));   // principal
-    scene.lights.push_back(Light(Vec3(-1.5f, 0.8f, -4.5f), 2.0f));  // relleno izquierda
-    scene.lights.push_back(Light(Vec3(1.5f, 0.8f, -4.5f), 2.0f));   // relleno derecha
-    scene.lights.push_back(Light(Vec3(0.0f, 1.0f, -8.7f), 0.4f)); 
 
-    Camera camera(Vec3(0, 0, -2.0f));
+   // scene.lights.push_back(Light(Vec3(0.0f, 1.0f, -5.8f), 3.5f));   // principal
+   //scene.lights.push_back(Light(Vec3(-1.5f, 0.8f, -4.5f), 2.0f));  // relleno izquierda
+   //scene.lights.push_back(Light(Vec3(1.5f, 0.8f, -4.5f), 2.0f));   // relleno derecha
+   //scene.lights.push_back(Light(Vec3(0.0f, 1.0f, -8.7f), 0.4f)); 
+
 
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
@@ -204,7 +320,7 @@ int main() {
     }
 
     //guardo las imagenes
-    saveBMP("C:\\Users\\matie\\Desktop\\output.bmp", width, height, framebuffer);
+    saveBMP("C:\\Users\\juanp\\Desktop\\output.bmp", width, height, framebuffer);
 
     std::cout << "Render terminado: output.bmp" << std::endl;
 
